d87d0aba6dc6d347175407b56c55ef2f3ba25efe
diamond <topdeveloper97@gmail.com>
2023-04-12 23:05:57 +0300

    Add: Hero Page

--- components/_page/Hero/index.tsx
+++ components/_page/Hero/index.tsx
@@ -3,11 +3,15 @@ import Image from "next/image";
 
 import styles from "./styles.module.scss";
 import background from "../../../public/images/hero_bg.png";
+import Container from "../../_ui/Container";
 
 const Hero: React.FC = () => {
   return (
     <div className={styles.wrapper}>
       <Image className={styles.background} alt="hero_background" src={background} />
+      <Container className={styles.content}>
+        <h1 className={styles.heroTitle}>No-nonsense funding for tech companies in food and energy</h1>
+      </Container>
     </div>
   );
 };
